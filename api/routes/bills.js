95761c7f1fe8212e0989596be1a5387990aa312a
junsee <dat10319@gmail.com>
2022-04-17 17:47:05 +0700

    fix create bill, add router checkout bill :v

--- api/routes/bills.js
+++ api/routes/bills.js
@@ -1,41 +1,60 @@
 const router = require("express").Router()
 
 const Bill = require("../models/Bill.js")
+const Product = require("../models/Product.js")
 
 const verify = require("../middle/verify.js")
 
 // CREATE BILL
 router.post("/", verify, async (req, res) => {
     let addId = req.body
+
+    let products = req.body.products
+    let newproducts = []
+    for (x in products) {
+        let product = await Product.findById(products[x]._id)
+        newproducts.push(product)
+    }
+
     addId.userid = req.user.userId
+    addId.products = newproducts
     const newBill = await Bill(addId)
     try {
         const savedBill = await newBill.save()
-        res.status(200).json(savedBill)
+        res.status(200).json(newBill)
     } catch (err) {
         res.status(500).json(err)
     }
 })
 
-
-// DELETE BILL
-// router.delete("/:id", verify, async (req, res) => {
-//     try {
-//         const product = await Product.findById(req.params.id)
-//         if (product.userid === req.user.userId) {
-//             try {
-//                 await post.delete()
-//                 res.status(200).json({"Message": "Product has been deleted..."})
-//             } catch (err) {
-//                 res.status(500).json(err)
-//             }
-//         } else {
-//             res.status(401).json({"Message": "You can delete only your product!"})
-//         }
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// })
+// CHECK OUT
+router.put("/:id", verify, async (req, res) => {
+    try {
+        const bill = await Bill.findById(req.params.id)
+        if (bill.userid === req.user.userId) {
+            if (bill.checkout) {
+                res.status(200).json({"Message": "This Bill has been paid!"})
+            } else {
+                try {
+                    const updatedBill = await Bill.findByIdAndUpdate(
+                        req.params.id, 
+                        {
+                            checkout: req.body.checkout,
+                        }, 
+                        {new: true}
+                    )
+                    res.status(200).json(updatedBill)
+                } catch (err) {
+                    res.status(500).json(err)
+                }
+            }
+        } else {
+            res.status(401).json({"Message": "You can update only your Bill!"})
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 // GET BILL
 router.get("/:id", verify, async (req, res) => {
@@ -49,13 +68,21 @@ router.get("/:id", verify, async (req, res) => {
 
 // GET ALL BILL
 router.get("/", verify, async (req, res) => {
+    const checkout = req.query.checkout
     const page = parseInt(req.query.page)
     const limit = parseInt(req.query.limit)
 
     const startIndex = (page - 1) * limit
     const endIndex = page * limit
     const results = {}
-    const totalPage = await Bill.countDocuments({userid: req.user.userId}).exec()
+    let findObj = {userid: req.user.userId}
+
+    if (checkout === "false")  {
+        findObj.checkout = false
+    } else if (checkout === "true")  {
+        findObj.checkout = true
+    }
+    const totalPage = await Bill.countDocuments(findObj).exec()
     
     if (endIndex < totalPage) {
         results.next = {
@@ -72,7 +99,7 @@ router.get("/", verify, async (req, res) => {
     }
       
     try {
-        results.results = await Bill.find({userid: req.user.userId}).limit(limit).skip(startIndex).exec()
+        results.results = await Bill.find(findObj).limit(limit).skip(startIndex).exec()
         res.status(200).json(results)
     } catch (err) {
         res.status(500).json(err)
